4625768cb90f68bb6d40c749e40e0649e25e9061
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2021-11-21 22:21:39 +0200

    main rewritten as pure functions

--- main.py
+++ main.py
@@ -1,195 +1,125 @@
 import math
 import time
 # Defining Function
-def f(x):
-    return x**3-5*x-9
 
 
-def bisection(x0, x1, e,f):
+def bisection(lower_bound, upper_bound, error_tolerance, bisection_function):
+    if bisection_function(lower_bound) * bisection_function(upper_bound) > 0.0:
+        print('Given guess values do not bracket the root.')
+        print('Try Again with different guess values.')
     step = 1
     print('\n\n*** BISECTION METHOD IMPLEMENTATION ***')
     condition = True
     while condition:
-        x2 = (x0 + x1) / 2
-        print('Iteration-%d, x2 = %0.6f and f(x2) = %0.6f' % (step, x2, f(x2)))
+        middle = (lower_bound + upper_bound) / 2
+        print('Iteration-%d, x2 = %0.6f and f(x2) = %0.6f' % (step, middle, bisection_function(middle)))
 
-        if f(x0) * f(x2) < 0:
-            x1 = x2
+        if bisection_function(lower_bound) * bisection_function(middle) < 0:
+            upper_bound = middle
         else:
-            x0 = x2
+            lower_bound = middle
 
         step = step + 1
-        condition = abs(f(x2)) > e
+        condition = abs(bisection_function(middle)) > error_tolerance
 
-    print('\nRequired Root is : %0.8f' % x2)
-    return x2
+    print('\nRequired Root is : %0.8f' % middle)
+    return middle
+
 
 # Implementing False Position Method
-def falsePosition(x0,x1,e,f):
+def false_position(lower_bound, upper_bound, error_tolerance, false_position_function):
+    if false_position_function(lower_bound) * false_position_function(upper_bound) > 0.0:
+        print('Given guess values do not bracket the root.')
+        print('Try Again with different guess values.')
     step = 1
     print('\n\n*** FALSE POSITION METHOD IMPLEMENTATION ***')
     condition = True
     while condition:
-        x2 = x0 - (x1-x0) * f(x0)/( f(x1) - f(x0) )
-        print('Iteration-%d, x2 = %0.6f and f(x2) = %0.6f' % (step, x2, f(x2)))
+        middle = lower_bound - (upper_bound-lower_bound) * false_position_function(lower_bound)/(false_position_function(upper_bound) - false_position_function(lower_bound))
+        print('Iteration-%d, x2 = %0.6f and f(x2) = %0.6f' % (step, middle, false_position_function(middle)))
 
-        if f(x0) * f(x2) < 0:
-            x1 = x2
+        if false_position_function(lower_bound) * false_position_function(middle) < 0:
+            upper_bound = middle
         else:
-            x0 = x2
+            lower_bound = middle
 
         step = step + 1
-        condition = abs(f(x2)) > e
+        condition = abs(false_position_function(middle)) > error_tolerance
 
-    print('\nRequired root is: %0.8f' % x2)
-    return x2
-
-
-def h(x):
-    return x * x * x + x * x - 1
-
-
-# Re-writing f(x)=0 to x = g(x)
-def g(x):
-    return 1 / math.sqrt(1 + x)
+    print('\nRequired root is: %0.8f' % middle)
+    return middle
 
 
 # Implementing Fixed Point Iteration Method
-def fixedPointIteration(X, E, N):
+def fixed_point_iteration(initial_guess_fixed_point, error_tolerance, maximum_step,fixed_point_function,fixed_point_rewritten_function):
     print('\n\n*** FIXED POINT ITERATION ***')
     step = 1
     flag = 1
     condition = True
     while condition:
-        xn = g(X)
-        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, xn, h(xn)))
-        X = xn
+        new_value_fixed_point = fixed_point_rewritten_function(initial_guess_fixed_point)
+        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, new_value_fixed_point, fixed_point_function(new_value_fixed_point)))
+        initial_guess_fixed_point = new_value_fixed_point
 
         step = step + 1
 
-        if step > N:
+        if step > maximum_step:
             flag = 0
             break
 
-        condition = abs(f(x1)) > E
+        condition = abs(fixed_point_function(new_value_fixed_point)) > error_tolerance
 
     if flag == 1:
-        print('\nRequired root is: %0.8f' % xn)
+        print('\nRequired root is: %0.8f' % new_value_fixed_point)
     else:
         print('\nNot Convergent.')
 
 
-def newtonRaphson(X, E, N):
+def newton_raphson(initial_guess_newton_raphson, error_tolerance, maximum_step,newton_raphson_function,newton_raphson_rewritten_function):
     print('\n\n*** NEWTON RAPHSON METHOD IMPLEMENTATION ***')
     step = 1
     flag = 1
     condition = True
     while condition:
-        if g(X) == 0.0:
+        if newton_raphson_rewritten_function(initial_guess_newton_raphson) == 0.0:
             print('Divide by zero error!')
             break
 
-        xn = X - h(X) / g(X)
-        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, xn, f(xn)))
-        X = xn
+        new_value_newton_raphson = initial_guess_newton_raphson - newton_raphson_function(initial_guess_newton_raphson) / newton_raphson_rewritten_function(initial_guess_newton_raphson)
+        print('Iteration-%d, x1 = %0.6f and f(x1) = %0.6f' % (step, new_value_newton_raphson, newton_raphson_function(new_value_newton_raphson)))
+        initial_guess_newton_raphson = new_value_newton_raphson
         step = step + 1
 
-        if step > N:
+        if step > maximum_step:
             flag = 0
             break
 
-        condition = abs(f(x1)) > E
+        condition = abs(newton_raphson_function(new_value_newton_raphson)) > error_tolerance
 
     if flag == 1:
-        print('\nRequired root is: %0.8f' % x1)
+        print('\nRequired root is: %0.8f' % new_value_newton_raphson)
     else:
         print('\nNot Convergent.')
 
-def take_input():
-    # Input Section for fixed
-    X = input('Enter Guess for fixed point and Newton Raphson: ')
-    E = input('Tolerable Error: ')
-    N = input('Maximum Step: ')
 
-    # Converting x0 and e to float
-    X = float(X)
-    E = float(E)
-
-    # Converting N to integer
-    N = int(N)
-
-    # Input Section for bisection and false position
-    x0 = input('First Guess for bisection and false position: ')
-    x1 = input('Second Guess for bisection and false position: ')
-    e = input('Tolerable Error for bisection and false position: ')
-
-    # Converting input to float
-    x0 = float(x0)
-    x1 = float(x1)
-    e = float(e)
-
-
-def c(x):
-    return x ** 3 - 5 * x - 9
-
-
-# Implementing Secant Method
-
-def secant(xs, xm, e1, N1):
+def secant(xi, ximinus1, error_tolerance, maximum_step,secant_function):
     print('\n\n*** SECANT METHOD IMPLEMENTATION ***')
     step = 1
     condition = True
     while condition:
-        if c(xs) == c(xm):
+        if secant_function(xi) == secant_function(ximinus1):
             print('Divide by zero error!')
             break
 
-        x2 = xs - (xm - xs) * c(xs) / (c(xm) - c(xs))
-        print('Iteration-%d, x2 = %0.6f and f(x2) = %0.6f' % (step, x2, c(x2)))
-        xs = xm
-        xm = x2
+        xiplus1 = xi - (ximinus1 - xi) * secant_function(xi) / (secant_function(ximinus1) - secant_function(xi))
+        print('Iteration-%d, x2 = %0.6f and f(x2) = %0.6f' % (step, xiplus1, secant_function(xiplus1)))
+        xi = ximinus1
+        ximinus1 = xiplus1
         step = step + 1
 
-        if step > N1:
+        if step > maximum_step:
             print('Not Convergent!')
             break
 
-        condition = abs(c(x2)) > e1
-    print('\n Required root is: %0.8f' % x2)
-
-def main():
-    # Input Section
-    xs = input('Enter First Guess for secant: ')
-    xm = input('Enter Second Guess for secant: ')
-    e1 = input('Tolerable Error for secant: ')
-    N1 = input('Maximum Step for secant: ')
-
-    # Converting x0 and e to float
-    xs = float(xs)
-    xm = float(xm)
-    e1 = float(e1)
-
-    # Converting N to integer
-    N1 = int(N1)
-
-    # Checking Correctness of initial guess values and false positioning
-    if f(x0) * f(x1) > 0.0:
-        print('Given guess values do not bracket the root.')
-        print('Try Again with different guess values.')
-    else:
-        startfalse= time.time();
-        falsePosition(x0,x1,e)
-        print("time for false position is " ,time.time()-startfalse);
-        startbi = time.time();
-        bisection(x0,x1,e)
-        print("time for bisection is ", time.time() - startbi);
-        startfixed = time.time();
-        fixedPointIteration(X, E, N)
-        print("time for fixed is ", time.time() - startfixed);
-        startnewton = time.time();
-        newtonRaphson(X, E, N)
-        print("time for newton raphson is ", time.time() - startnewton);
-
-        startsecant=time.time();
-    secant(xs,xm,e1,N1)
-    print("time for secant is ", time.time() - startsecant);
+        condition = abs(secant_function(xiplus1)) > error_tolerance
+    print('\n Required root is: %0.8f' % xiplus1)
