11c038076d3bf9ad674a73b8e37ff92d9688bcb7
Heba Elwazzan <hebaelwazan@gmail.com>
2021-11-30 23:24:54 +0200

    polished simulation up

--- simulation.py
+++ simulation.py
@@ -3,22 +3,29 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-i = 0
 
 def simulate(df, func, xrange, step):
-    duration = 2 # in sec
-    refreshPeriod = 750 # in ms
-    def startanim(frame, l, p1, p2, p3):
-        global i
-        if i < l:
-            x1 = df['x`'].iloc[i]
-            p1.set_xdata([x1,x1])
-            x2 = df['xl'].iloc[i]
-            p2.set_xdata([x2,x2])
-            x3 = df['xu'].iloc[i]
-            p3.set_xdata([x3,x3])
-            plt.xlim([x2 - 0.1, x3 + 0.1])
-            i += 1
+    duration = len(df.index)/10 # in sec
+    refreshPeriod = 100 # in ms
+    def startanim(frame, iterator, p1, p2, p3):
+        try:
+            i = next(iterator)
+            if i < len(df.index):
+                x1 = df['x`'].iloc[i]
+                p1.set_xdata([x1,x1])
+                x2 = df['xl'].iloc[i]
+                p2.set_xdata([x2,x2])
+                x3 = df['xu'].iloc[i]
+                p3.set_xdata([x3,x3])
+                plt.xlim([x2 - 0.1, x3 + 0.1])
+
+        except StopIteration:
+            # iterator = iter(range(len(df.index)))
+            root = df["x`"].iloc[-1]
+            root_value = df["f(x`)"].iloc[-1]
+            #plt.xlim([root - 0.000001, root + 0.000001])
+            annotation_root = ax.annotate("x` = " + str(root), xy=(root, root_value), xytext=(root, root_value))
+
 
 
 
@@ -26,7 +33,7 @@ def simulate(df, func, xrange, step):
     f = []
     for x_ in x:
         f.append(func(x_))
-    fig = plt.figure()
+    fig, ax = plt.subplots()
     # plt.xticks(rotation=45, ha="right", rotation_mode="anchor")
     plt.subplots_adjust(bottom = 0.2, top = 0.9)
     plt.ylabel('f(x)')
@@ -36,6 +43,9 @@ def simulate(df, func, xrange, step):
     p2 = plt.axvline(x=df['xl'].iloc[0], color='green')
     p3 = plt.axvline(x=df['xu'].iloc[0], color='blue')
     plt.legend(['xu', 'x`', 'xl'])
-    animator = ani.FuncAnimation(fig, startanim,  frames=int(duration/(refreshPeriod/1000)), fargs=(len(df.index), p1, p2, p3), interval=refreshPeriod)
+
+    animator = ani.FuncAnimation(fig, startanim,  frames=int(duration/(refreshPeriod/1000)), fargs=(iter(range(len(df.index))), p1, p2, p3), interval=refreshPeriod)
+
     plt.show()
+
 
